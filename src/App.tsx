1b3379b5ee0e269d2f38c7e9c94f828c15e39867
Dip3102001 <dip3102001@gmail.com>
2025-03-14 11:44:26 -0400

    Updated : app.tsx was updated to enable dark mode as default

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import MainContent from './components/Main';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -13,6 +13,11 @@ import ChatbotWidget from './components/Chatbot';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme','dark');
+  })
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-200 p-4">
     
