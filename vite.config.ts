43aa3101adda0d6f514773bdedd3113f935b7d6b
Dip3102001 <dip3102001@gmail.com>
2025-03-21 16:08:57 -0400

    Change : Vite file to enable REST API call to chat

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  server:{
+    proxy: {
+      '/api/proxy': {
+        target: 'http://3.143.254.129',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/proxy/, '/api/answer')
+      }
+    }
+  }
 })
